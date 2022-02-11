d97d466e1a9e632aa67887d6804a768242435aba
Higor <higorpiresoliveira@gmail.com>
2022-02-11 17:39:25 -0300

    ensure RemoteAuthentication throws InvalidCredentialsError on 401

--- lib/data/usecases/remote_authentication.dart
+++ lib/data/usecases/remote_authentication.dart
@@ -18,8 +18,8 @@ class RemoteAuthentication {
         method: 'POST',
         body: RemoteAuthenticationParams.fromDomain(params).toJson(),
       );
-    } on HttpError {
-      throw DomainError.unexpected;
+    } on HttpError catch (error) {
+      throw error == HttpError.unauthorized ? DomainError.invalidCredentials : DomainError.unexpected;
     }
   }
 }
