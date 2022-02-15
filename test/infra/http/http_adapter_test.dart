184e18a08efbdb4e930fbd4ef4e40f8770ce46d6
Higor <higorpiresoliveira@gmail.com>
2022-02-15 09:19:42 -0300

    ensure HttpAdapter returns null if post returns 200 with no data

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -70,5 +70,12 @@ void main() {
 
       expect(response, {'any_key': 'any_value'});
     });
+
+    test('Should return null if post returns 200 with no data', () async {
+      when(client.post(any, headers: anyNamed('headers'))).thenAnswer((_) async => Response('', 200));
+      final response = await sut.request(url: url, method: 'post');
+
+      expect(response, null);
+    });
   });
 }
