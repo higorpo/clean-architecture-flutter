996f41c275783954d6bcdbed8fcd33d67d5c4d97
Higor <higorpiresoliveira@gmail.com>
2022-02-18 10:45:36 -0300

    ensure LoginPage presents error message if authentication fails

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -39,6 +39,17 @@ class LoginPage extends StatelessWidget {
             }
           });
 
+          presenter.mainErrorStream.listen((error) {
+            if (error != null) {
+              Scaffold.of(context).showSnackBar(
+                SnackBar(
+                  backgroundColor: Colors.red[900],
+                  content: Text(error, textAlign: TextAlign.center),
+                ),
+              );
+            }
+          });
+
           return SingleChildScrollView(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
