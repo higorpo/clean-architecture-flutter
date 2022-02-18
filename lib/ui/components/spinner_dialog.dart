eed394134fe7ee2bafb1aa7366035ccef76aff70
Higor <higorpiresoliveira@gmail.com>
2022-02-18 10:57:16 -0300

    move SpinnerDialog to its own file

---
+++ lib/ui/components/spinner_dialog.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void showLoading(BuildContext context) {
+  showDialog(
+    context: context,
+    barrierDismissible: false,
+    child: SimpleDialog(
+      children: [
+        Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            CircularProgressIndicator(),
+            SizedBox(height: 10),
+            Text('Aguarde...', textAlign: TextAlign.center),
+          ],
+        ),
+      ],
+    ),
+  );
+}
+
+void hideLoading(BuildContext context) {
+  if (Navigator.canPop(context)) {
+    Navigator.pop(context);
+  }
+}
--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -27,26 +27,9 @@ class _LoginPageState extends State<LoginPage> {
         builder: (context) {
           widget.presenter.isLoadingStream.listen((isLoading) {
             if (isLoading) {
-              showDialog(
-                context: context,
-                barrierDismissible: false,
-                child: SimpleDialog(
-                  children: [
-                    Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        CircularProgressIndicator(),
-                        SizedBox(height: 10),
-                        Text('Aguarde...', textAlign: TextAlign.center),
-                      ],
-                    ),
-                  ],
-                ),
-              );
+              showLoading(context);
             } else {
-              if (Navigator.canPop(context)) {
-                Navigator.pop(context);
-              }
+              hideLoading(context);
             }
           });
 
