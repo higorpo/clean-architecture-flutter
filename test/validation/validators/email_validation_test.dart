1bba54ca348bd70e640f82bb152f7312511f28f6
Higor <higorpiresoliveira@gmail.com>
2022-02-22 08:44:42 -0300

    move EmailValidation to its own file

--- test/validation/validators/email_validation_test.dart
+++ test/validation/validators/email_validation_test.dart
@@ -1,13 +1,6 @@
-import 'package:ForDev/validation/protocols/protocols.dart';
 import 'package:test/test.dart';
 
-class EmailValidation implements FieldValidation {
-  String validate(String value) {
-    final regex = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
-    final isValid = value?.isNotEmpty != true || regex.hasMatch(value);
-    return isValid ? null : "Campo inválido";
-  }
-}
+import 'package:ForDev/validation/validators/validators.dart';
 
 void main() {
   EmailValidation sut;
