faeb3fa884f693110f7705519e933d9d24b18f40
Higor <higorpiresoliveira@gmail.com>
2022-03-02 08:09:09 -0300

    add GetxLoginPresenter

--- lib/main/factories/pages/login/login_presenter_factory.dart
+++ lib/main/factories/pages/login/login_presenter_factory.dart
@@ -2,8 +2,15 @@ import '../../../../presentation/presenters/presenters.dart';
 import '../../../../ui/pages/pages.dart';
 import '../../factories.dart';
 
-LoginPresenter makeLoginPresenter() {
+LoginPresenter makeStreamLoginPresenter() {
   return StreamLoginPresenter(
+    validation: makeLoginValidation(),
+    authentication: makeRemoteAuthentication(),
+  );
+}
+
+LoginPresenter makeGetxLoginPresenter() {
+  return GetxLoginPresenter(
     validation: makeLoginValidation(),
     authentication: makeRemoteAuthentication(),
   );
