fcd7b981913a55d145774237b0e9fe50d277050b
Higor <higorpiresoliveira@gmail.com>
2022-03-07 11:05:28 -0300

    ensure LoginPage send user to correct page on login

--- lib/main/factories/pages/login/login_presenter_factory.dart
+++ lib/main/factories/pages/login/login_presenter_factory.dart
@@ -2,12 +2,12 @@ import '../../../../presentation/presenters/presenters.dart';
 import '../../../../ui/pages/pages.dart';
 import '../../factories.dart';
 
-LoginPresenter makeStreamLoginPresenter() {
-  return StreamLoginPresenter(
-    validation: makeLoginValidation(),
-    authentication: makeRemoteAuthentication(),
-  );
-}
+// LoginPresenter makeStreamLoginPresenter() {
+//   return StreamLoginPresenter(
+//     validation: makeLoginValidation(),
+//     authentication: makeRemoteAuthentication(),
+//   );
+// }
 
 LoginPresenter makeGetxLoginPresenter() {
   return GetxLoginPresenter(
--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:provider/provider.dart';
 
 import '../../components/components.dart';
@@ -6,36 +7,25 @@ import '../../components/components.dart';
 import 'components/components.dart';
 import 'login_presenter.dart';
 
-class LoginPage extends StatefulWidget {
+class LoginPage extends StatelessWidget {
   final LoginPresenter presenter;
 
   const LoginPage(this.presenter);
 
   @override
-  _LoginPageState createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  void _hideKeyboard() {
-    final currentFocus = FocusScope.of(context);
-
-    if (!currentFocus.hasPrimaryFocus) {
-      currentFocus.unfocus();
-    }
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    widget.presenter.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
+    void _hideKeyboard() {
+      final currentFocus = FocusScope.of(context);
+
+      if (!currentFocus.hasPrimaryFocus) {
+        currentFocus.unfocus();
+      }
+    }
+
     return Scaffold(
       body: Builder(
         builder: (context) {
-          widget.presenter.isLoadingStream.listen((isLoading) {
+          presenter.isLoadingStream.listen((isLoading) {
             if (isLoading) {
               showLoading(context);
             } else {
@@ -43,9 +33,15 @@ class _LoginPageState extends State<LoginPage> {
             }
           });
 
-          widget.presenter.mainErrorStream.listen((error) {
+          presenter.mainErrorStream.listen((error) {
             if (error != null) {
               showErrorMessage(context, error);
+            }
+          });
+
+          presenter.navigateToStream.listen((page) {
+            if (page?.isNotEmpty == true) {
+              Get.offAllNamed(page);
             }
           });
 
@@ -60,7 +56,7 @@ class _LoginPageState extends State<LoginPage> {
                   Padding(
                     padding: const EdgeInsets.all(32),
                     child: Provider(
-                      create: (_) => widget.presenter,
+                      create: (_) => presenter,
                       child: Form(
                         child: Column(
                           children: [
