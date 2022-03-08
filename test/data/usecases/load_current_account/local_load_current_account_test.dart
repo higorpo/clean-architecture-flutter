cabf8cf3b7c7f0c43e2dcf461bc5a2ad3cbae766
Higor <higorpiresoliveira@gmail.com>
2022-03-08 09:04:11 -0300

    ensure LocalLoadCurrentAccount calls FetchSecureCacheStorage with correct value

---
+++ test/data/usecases/load_current_account/local_load_current_account_test.dart
@@ -0,0 +1,30 @@
+import 'package:test/test.dart';
+import 'package:meta/meta.dart';
+import 'package:mockito/mockito.dart';
+
+class LocalLoadCurrentAccount {
+  final FetchSecureCacheStorage fetchSecureCacheStorage;
+
+  LocalLoadCurrentAccount({@required this.fetchSecureCacheStorage});
+
+  Future<void> load() async {
+    await fetchSecureCacheStorage.fetchSecure('token');
+  }
+}
+
+abstract class FetchSecureCacheStorage {
+  Future<void> fetchSecure(String key);
+}
+
+class FetchSecureCacheStorageSpy extends Mock implements FetchSecureCacheStorage {}
+
+void main() {
+  test('Should call FetchSecureCacheStorage with correct value', () async {
+    final fetchSecureCacheStorage = FetchSecureCacheStorageSpy();
+    final sut = LocalLoadCurrentAccount(fetchSecureCacheStorage: fetchSecureCacheStorage);
+
+    await sut.load();
+
+    verify(fetchSecureCacheStorage.fetchSecure('token')).called(1);
+  });
+}
