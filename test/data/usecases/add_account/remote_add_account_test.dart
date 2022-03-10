62d8c62e7544550b123b7176c5649ba0b7eb06b5
Higor <higorpiresoliveira@gmail.com>
2022-03-10 09:28:23 -0300

    ensure RemoteAddAccount returns an Account on 200

--- test/data/usecases/add_account/remote_add_account_test.dart
+++ test/data/usecases/add_account/remote_add_account_test.dart
@@ -16,7 +16,16 @@ void main() {
   String url;
   AddAccountParams params;
 
+  Map mockValidData() => {
+        'accessToken': faker.guid.guid(),
+        'name': faker.person.name(),
+      };
+
   PostExpectation mockRequest() => when(httpClient.request(url: anyNamed('url'), method: anyNamed('method'), body: anyNamed('body')));
+
+  void mockHttpData(Map data) {
+    mockRequest().thenAnswer((_) async => data);
+  }
 
   void mockHttpError(HttpError error) {
     mockRequest().thenThrow(error);
@@ -32,6 +41,7 @@ void main() {
       password: faker.internet.password(),
       passwordConfirmation: faker.internet.password(),
     );
+    mockHttpData(mockValidData());
   });
 
   test('Should call HttpClient with correct values', () async {
@@ -79,5 +89,14 @@ void main() {
     final future = sut.add(params);
 
     expect(future, throwsA(DomainError.emailInUse));
+  });
+
+  test('Should return an Account if HttpClient returns 200', () async {
+    final validData = mockValidData();
+    mockHttpData(validData);
+
+    final account = await sut.add(params);
+
+    expect(account.token, validData['accessToken']);
   });
 }
