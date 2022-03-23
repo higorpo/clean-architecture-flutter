cd15ce424e91a07e85f1e6cf71c3345c752257ad
Higor <higorpiresoliveira@gmail.com>
2022-03-23 09:29:42 -0300

    ensure GetxSignUpPresenter emits correct error on name changed

--- lib/presentation/presenters/getx_signup_presenter.dart
+++ lib/presentation/presenters/getx_signup_presenter.dart
@@ -9,9 +9,11 @@ class GetxSignUpPresenter extends GetxController {
   final Validation validation;
 
   var _emailError = Rx<UIError>();
+  var _nameError = Rx<UIError>();
   var _isFormValid = false.obs;
 
   Stream<UIError> get emailErrorStream => _emailError.stream;
+  Stream<UIError> get nameErrorStream => _nameError.stream;
   Stream<bool> get isFormValidStream => _isFormValid.stream;
 
   GetxSignUpPresenter({@required this.validation});
@@ -21,6 +23,11 @@ class GetxSignUpPresenter extends GetxController {
 
   void validateEmail(String email) {
     _emailError.value = _validateField(field: 'email', value: email);
+    _validateForm();
+  }
+
+  void validateName(String name) {
+    _nameError.value = _validateField(field: 'name', value: name);
     _validateForm();
   }
 
