905317f8a903f7ab52a35c653936d6d9bdf0f6f6
Higor <higorpiresoliveira@gmail.com>
2022-03-23 09:03:01 -0300

    ensure SignUpPage enable submit button if form is valid

--- lib/ui/pages/signup/components/signup_button.dart
+++ lib/ui/pages/signup/components/signup_button.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../../helpers/helpers.dart';
+import '../signup_presenter.dart';
 
 class SignUpButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return RaisedButton(
-      child: Text(R.strings.enter.toUpperCase()),
-      onPressed: null,
+    final presenter = Provider.of<SignUpPresenter>(context);
+
+    return StreamBuilder<bool>(
+      stream: presenter.isFormValidStream,
+      builder: (context, snapshot) {
+        return RaisedButton(
+          child: Text(R.strings.addAccount.toUpperCase()),
+          onPressed: snapshot.data == true ? presenter.signUp : null,
+        );
+      },
     );
   }
 }
