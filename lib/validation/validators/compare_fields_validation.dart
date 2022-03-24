fb4466c90aead519e5ac9239ccd238e9619e43e6
Higor <higorpiresoliveira@gmail.com>
2022-03-24 09:25:52 -0300

    ensure CompareFieldsValidation returns null if values are equal

--- lib/validation/validators/compare_fields_validation.dart
+++ lib/validation/validators/compare_fields_validation.dart
@@ -12,6 +12,6 @@ class CompareFieldsValidation implements FieldValidation {
 
   @override
   ValidationError validate(String value) {
-    return ValidationError.invalidField;
+    return value == valueToCompare ? null : ValidationError.invalidField;
   }
 }
