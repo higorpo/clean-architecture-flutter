4ec61695dfb2399d9a95dfede67666bf7a5430d2
Higor <higorpiresoliveira@gmail.com>
2022-04-19 09:30:39 -0300

    ensure SurveysPage handles loading correctly

--- lib/ui/pages/surveys/surveys_page.dart
+++ lib/ui/pages/surveys/surveys_page.dart
@@ -1,6 +1,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
+import '../../components/components.dart';
 import '../../helpers/helpers.dart';
 
 import 'components/components.dart';
@@ -19,18 +20,28 @@ class SurveysPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(R.strings.surveys),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 20),
-        child: CarouselSlider(
-          items: [
-            SurveyItem(),
-          ],
-          options: CarouselOptions(
-            enlargeCenterPage: true,
-            aspectRatio: 1,
+      body: Builder(builder: (context) {
+        presenter.isLoadingStream.listen((isLoading) {
+          if (isLoading == true) {
+            showLoading(context);
+          } else {
+            hideLoading(context);
+          }
+        });
+
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20),
+          child: CarouselSlider(
+            items: [
+              SurveyItem(),
+            ],
+            options: CarouselOptions(
+              enlargeCenterPage: true,
+              aspectRatio: 1,
+            ),
           ),
-        ),
-      ),
+        );
+      }),
     );
   }
 }
