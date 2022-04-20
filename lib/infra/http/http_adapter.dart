05344e88457b865954fe526b71200c048829dd2a
Higor <higorpiresoliveira@gmail.com>
2022-04-20 10:14:40 -0300

    add headers to HttpClient

--- lib/infra/http/http_adapter.dart
+++ lib/infra/http/http_adapter.dart
@@ -10,24 +10,26 @@ class HttpAdapter implements HttpClient {
 
   HttpAdapter(this.client);
 
-  Future<Map> request({
+  Future<dynamic> request({
     @required String url,
     @required String method,
     Map body,
+    Map headers,
   }) async {
-    final headers = {
-      'content-type': 'application/json',
-      'accept': 'application/json',
-    };
+    final defaultHeaders = headers?.cast<String, String>() ?? {}
+      ..addAll({
+        'content-type': 'application/json',
+        'accept': 'application/json',
+      });
     final jsonBody = body != null ? jsonEncode(body) : null;
 
     var response = Response('', 500);
 
     try {
       if (method == 'post') {
-        response = await client.post(url, headers: headers, body: jsonBody);
+        response = await client.post(url, headers: defaultHeaders, body: jsonBody);
       } else if (method == 'get') {
-        response = await client.get(url, headers: headers);
+        response = await client.get(url, headers: defaultHeaders);
       }
     } catch (error) {
       throw HttpError.serverError;
@@ -36,7 +38,7 @@ class HttpAdapter implements HttpClient {
     return _handleResponse(response);
   }
 
-  Map _handleResponse(Response response) {
+  dynamic _handleResponse(Response response) {
     if (response.statusCode == 200) {
       return response.body.isNotEmpty ? jsonDecode(response.body) : null;
     } else if (response.statusCode == 204) {
