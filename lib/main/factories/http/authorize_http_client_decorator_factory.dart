a16b26b010472cd1dd6a88bba9120883cdd2944e
Higor <higorpiresoliveira@gmail.com>
2022-04-25 09:02:34 -0300

    add AuthorizeHttpClientDecorator factory

---
+++ lib/main/factories/http/authorize_http_client_decorator_factory.dart
@@ -0,0 +1,6 @@
+import '../../../data/http/http.dart';
+import '../../factories/factories.dart';
+import '../../decorators/decorators.dart';
+
+HttpClient makeAuthorizeHttpClientDecorator() =>
+    AuthorizeHttpClientDecorator(decoratee: makeHttpAdapter(), fetchSecureCacheStorage: makeLocalStorageAdapter());
--- lib/main/factories/usecases/load_surveys_factory.dart
+++ lib/main/factories/usecases/load_surveys_factory.dart
@@ -4,5 +4,5 @@ import '../../../domain/usecases/usecases.dart';
 import '../factories.dart';
 
 LoadSurveys makeRemoteLoadSurveys() {
-  return RemoteLoadSurveys(httpClient: makeHttpAdapter(), url: makeApiUrl('surveys'));
+  return RemoteLoadSurveys(httpClient: makeAuthorizeHttpClientDecorator(), url: makeApiUrl('surveys'));
 }
