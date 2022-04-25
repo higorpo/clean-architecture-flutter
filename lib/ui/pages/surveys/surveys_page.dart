bf3b0148217e92bdcc9486fb1f4b9d724248dcd9
Higor <higorpiresoliveira@gmail.com>
2022-04-25 09:07:07 -0300

    move loadData after stream listen

--- lib/ui/pages/surveys/surveys_page.dart
+++ lib/ui/pages/surveys/surveys_page.dart
@@ -15,8 +15,6 @@ class SurveysPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    presenter.loadData();
-
     return Scaffold(
       appBar: AppBar(
         title: Text(R.strings.surveys),
@@ -29,6 +27,8 @@ class SurveysPage extends StatelessWidget {
             hideLoading(context);
           }
         });
+
+        presenter.loadData();
 
         return StreamBuilder<List<SurveyViewModel>>(
             stream: presenter.surveysStream,
