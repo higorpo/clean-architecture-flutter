ccd7068765481c0450892cb8f5bcb411fab2f25c
Higor <higorpiresoliveira@gmail.com>
2022-05-02 09:22:04 -0300

    move code to their own files

---
+++ lib/data/usecases/load_surveys/local_load_surveys.dart
@@ -0,0 +1,27 @@
+import 'package:meta/meta.dart';
+
+import '../../../domain/entities/entities.dart';
+import '../../../domain/helpers/helpers.dart';
+import '../../../domain/usecases/usecases.dart';
+import '../../cache/cache.dart';
+import '../../models/models.dart';
+
+class LocalLoadSurveys implements LoadSurveys {
+  final FetchCacheStorage fetchCacheStorage;
+
+  LocalLoadSurveys({@required this.fetchCacheStorage});
+
+  Future<List<SurveyEntity>> load() async {
+    try {
+      final data = await fetchCacheStorage.fetch('surveys');
+
+      if (data?.isEmpty != false) {
+        throw Exception();
+      }
+
+      return data.map<SurveyEntity>((json) => LocalSurveyModel.fromJson(json).toEntity()).toList();
+    } catch (_) {
+      throw DomainError.unexpected;
+    }
+  }
+}
