7e91c79eeb4241028fcf3161b514b4a510ac6463
Higor <higorpiresoliveira@gmail.com>
2022-06-22 09:28:54 -0300

    ensure SurveyResultPage calls LoadSurveyResult on reload button

--- test/ui/pages/survey_result_page_test.dart
+++ test/ui/pages/survey_result_page_test.dart
@@ -89,4 +89,15 @@ void main() {
     expect(find.text('Algo errado aconteceu. Tente novamente em breve.'), findsOneWidget);
     expect(find.text('Recarregar'), findsOneWidget);
   });
+
+  testWidgets('Should call LoadSurveyResult on reload button', (WidgetTester tester) async {
+    await loadPage(tester);
+
+    surveyResultController.addError(UIError.unexpected.description);
+    await tester.pump();
+
+    await tester.tap(find.text('Recarregar'));
+
+    verify(presenter.loadData()).called(2);
+  });
 }
