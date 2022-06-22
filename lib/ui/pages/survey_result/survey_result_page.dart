7086544de2713df34fe7e3aaf947822be13376d2
Higor <higorpiresoliveira@gmail.com>
2022-06-22 08:59:56 -0300

    ensure SurveyResultPage calls LoadSurveyResult on page load

--- lib/ui/pages/survey_result/survey_result_page.dart
+++ lib/ui/pages/survey_result/survey_result_page.dart
@@ -2,67 +2,79 @@ import 'package:flutter/material.dart';
 
 import '../../helpers/helpers.dart';
 
+import 'survey_result_presenter.dart';
+
 class SurveyResultPage extends StatelessWidget {
+  final SurveyResultPresenter presenter;
+
+  const SurveyResultPage({@required this.presenter});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(R.strings.surveys)),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          if (index == 0) {
-            return Container(
-              padding: const EdgeInsets.only(top: 40, bottom: 20, left: 20, right: 20),
-              decoration: BoxDecoration(
-                color: Theme.of(context).disabledColor.withAlpha(90),
-              ),
-              child: Text('Qual é seu framework web favorito?'),
-            );
-          }
+      body: Builder(
+        builder: (context) {
+          presenter.loadData();
 
-          return Column(
-            children: [
-              Container(
-                padding: const EdgeInsets.all(15),
-                decoration: BoxDecoration(color: Theme.of(context).backgroundColor),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Image.network(
-                      'http://fordevs.herokuapp.com/static/img/logo-react.png',
-                      width: 40,
-                    ),
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10),
-                        child: Text(
-                          'React',
-                          style: TextStyle(fontSize: 16),
+          return ListView.builder(
+            itemBuilder: (context, index) {
+              if (index == 0) {
+                return Container(
+                  padding: const EdgeInsets.only(top: 40, bottom: 20, left: 20, right: 20),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).disabledColor.withAlpha(90),
+                  ),
+                  child: Text('Qual é seu framework web favorito?'),
+                );
+              }
+
+              return Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(15),
+                    decoration: BoxDecoration(color: Theme.of(context).backgroundColor),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Image.network(
+                          'http://fordevs.herokuapp.com/static/img/logo-react.png',
+                          width: 40,
                         ),
-                      ),
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            child: Text(
+                              'React',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ),
+                        ),
+                        Text(
+                          '100%',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Theme.of(context).primaryColorDark,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 10),
+                          child: Icon(
+                            Icons.check_circle,
+                            color: Theme.of(context).highlightColor,
+                          ),
+                        )
+                      ],
                     ),
-                    Text(
-                      '100%',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Theme.of(context).primaryColorDark,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 10),
-                      child: Icon(
-                        Icons.check_circle,
-                        color: Theme.of(context).highlightColor,
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              Divider(height: 1),
-            ],
+                  ),
+                  Divider(height: 1),
+                ],
+              );
+            },
+            itemCount: 4,
           );
         },
-        itemCount: 4,
       ),
     );
   }
---
+++ test/ui/pages/survey_result_page_test.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get/get.dart';
+import 'package:image_test_utils/image_test_utils.dart';
+import 'package:mockito/mockito.dart';
+
+import 'package:ForDev/ui/pages/pages.dart';
+
+class SurveyResultPresenterSpy extends Mock implements SurveyResultPresenter {}
+
+void main() {
+  SurveyResultPresenterSpy presenter;
+
+  Future<void> loadPage(WidgetTester tester) async {
+    presenter = SurveyResultPresenterSpy();
+
+    final surveyResult = GetMaterialApp(
+      initialRoute: '/survey_result/any_survey_id',
+      getPages: [
+        GetPage(name: '/survey_result/:survey_id', page: () => SurveyResultPage(presenter: presenter)),
+      ],
+    );
+    provideMockedNetworkImages(() async {
+      await tester.pumpWidget(surveyResult);
+    });
+  }
+
+  testWidgets('Should call LoadSurveyResult on page load', (WidgetTester tester) async {
+    await loadPage(tester);
+
+    verify(presenter.loadData()).called(1);
+  });
+}
