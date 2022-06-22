3419e876aeee44f29903f611c2237677899a5958
Higor <higorpiresoliveira@gmail.com>
2022-06-22 09:18:31 -0300

    move SurveyItems component to its own file

---
+++ lib/ui/pages/surveys/components/survey_items.dart
@@ -0,0 +1,29 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+import '../surveys.dart';
+
+import 'components.dart';
+
+class SurveyItems extends StatelessWidget {
+  final List<SurveyViewModel> viewModels;
+
+  const SurveyItems({
+    Key key,
+    @required this.viewModels,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      child: CarouselSlider(
+        items: viewModels.map((viewModel) => SurveyItem(viewModel)).toList(),
+        options: CarouselOptions(
+          enlargeCenterPage: true,
+          aspectRatio: 1,
+        ),
+      ),
+    );
+  }
+}
--- lib/ui/pages/surveys/surveys_page.dart
+++ lib/ui/pages/surveys/surveys_page.dart
@@ -1,4 +1,3 @@
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 import '../../components/components.dart';
@@ -31,27 +30,19 @@ class SurveysPage extends StatelessWidget {
         presenter.loadData();
 
         return StreamBuilder<List<SurveyViewModel>>(
-            stream: presenter.surveysStream,
-            builder: (context, snapshot) {
-              if (snapshot.hasError) {
-                return ReloadScreen(error: snapshot.error.toString(), reload: presenter.loadData);
-              }
+          stream: presenter.surveysStream,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return ReloadScreen(error: snapshot.error.toString(), reload: presenter.loadData);
+            }
 
-              if (snapshot.hasData) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 20),
-                  child: CarouselSlider(
-                    items: snapshot.data.map((viewModel) => SurveyItem(viewModel)).toList(),
-                    options: CarouselOptions(
-                      enlargeCenterPage: true,
-                      aspectRatio: 1,
-                    ),
-                  ),
-                );
-              }
+            if (snapshot.hasData) {
+              return SurveyItems(viewModels: snapshot.data);
+            }
 
-              return SizedBox(height: 0);
-            });
+            return SizedBox(height: 0);
+          },
+        );
       }),
     );
   }
