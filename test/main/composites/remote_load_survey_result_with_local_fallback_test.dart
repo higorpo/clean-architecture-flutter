34e2c60666745c3247e564c162076c0d2084af4e
Higor <higorpiresoliveira@gmail.com>
2022-07-12 10:49:18 -0300

    ensure Composite loads local data on remote failure

--- test/main/composites/remote_load_survey_result_with_local_fallback_test.dart
+++ test/main/composites/remote_load_survey_result_with_local_fallback_test.dart
@@ -15,10 +15,19 @@ class RemoteLoadSurveyResultWithLocalFallback implements LoadSurveyResult {
   RemoteLoadSurveyResultWithLocalFallback({@required this.remote, @required this.local});
 
   Future<SurveyResultEntity> loadBySurvey({String surveyId}) async {
-    final surveyResult = await remote.loadBySurvey(surveyId: surveyId);
-    await local.save(surveyId: surveyId, surveyResult: surveyResult);
+    try {
+      final surveyResult = await remote.loadBySurvey(surveyId: surveyId);
+      await local.save(surveyId: surveyId, surveyResult: surveyResult);
 
-    return surveyResult;
+      return surveyResult;
+    } catch (error) {
+      if (error == DomainError.accessDenied) {
+        rethrow;
+      }
+
+      await local.validate(surveyId);
+      await local.loadBySurvey(surveyId: surveyId);
+    }
   }
 }
 
@@ -87,5 +96,14 @@ void main() {
     final future = sut.loadBySurvey(surveyId: surveyId);
 
     expect(future, throwsA(DomainError.accessDenied));
+  });
+
+  test('Should call local LoadBySurvey on remote error', () async {
+    mockRemoteLoadError(DomainError.unexpected);
+
+    await sut.loadBySurvey(surveyId: surveyId);
+
+    verify(local.validate(surveyId)).called(1);
+    verify(local.loadBySurvey(surveyId: surveyId)).called(1);
   });
 }
