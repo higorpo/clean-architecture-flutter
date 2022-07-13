e6343290aacd65a778f77260ca7661c46abf23f6
Higor <higorpiresoliveira@gmail.com>
2022-07-13 09:31:13 -0300

    add put to HttpAdapter

--- lib/infra/http/http_adapter.dart
+++ lib/infra/http/http_adapter.dart
@@ -30,6 +30,8 @@ class HttpAdapter implements HttpClient {
         response = await client.post(url, headers: defaultHeaders, body: jsonBody).timeout(Duration(seconds: 10));
       } else if (method == 'get') {
         response = await client.get(url, headers: defaultHeaders).timeout(Duration(seconds: 10));
+      } else if (method == 'put') {
+        response = await client.put(url, headers: defaultHeaders, body: jsonBody).timeout(Duration(seconds: 10));
       }
     } catch (error) {
       throw HttpError.serverError;
